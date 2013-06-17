0254b4a582789d03a401ab4252bc058eeab3e924
Frank Hamand <frankhamand@gmail.com>
2013-06-17 23:09:36 +0100

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindNiceWeather::Application.config.secret_token = '0a8b06434e65ae1375dd31919574b8fbc0d6c2f1709cd7f5ad845916773c2304e193b2199b00a381ad7fbbe5deb5ce0088797338b69577647bfef637819e1f57'
