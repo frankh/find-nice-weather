97ed9cb96cc2f9dc8da686e0cf67e6d9c19c1433
Frank <frankhamand@hotmail.com>
2013-06-18 16:47:53 +0200

    updated weather script

--- script/get-weather.rb
+++ script/get-weather.rb
@@ -1,9 +1,10 @@
 require('net/http')
+require('json')
 
 api_key = "xey7xpg9qkgw6ccnfweycfwt"
 weather_url = "/free/v1/weather.ashx?key="
 weather_url += api_key
-puts weather_url
+weather_url += "&num_of_days=7"
 
 counter = 0
 File.open("weather-geocoded-uk-towns", "w") do |out|
@@ -11,9 +12,36 @@ File.open("weather-geocoded-uk-towns", "w") do |out|
 		# skip first line
 		fd.gets
 	    while (line = fd.gets)
-	        town, lat, long = line.split(/,/)
-	        req_url = weather_url + "&q=" + lat+","+long + "&format=json"
+	        town, lat, long = line.strip.split(/,/)
+		print town + "\n"
+	        req_url = weather_url + "&q=" + lat+","+long+"&format=json"
+		print req_url + "\n"
 	        resp = Net::HTTP.get_response('api.worldweatheronline.com', req_url)
-	    end
+		json_resp = JSON.parse(resp.body)
+		out.write(town)
+		out.write(',')
+		out.write(lat)
+		out.write(',')
+		out.write(long)
+		out.write('||')
+		json_resp["data"]["weather"].each { |w| 
+			out.write(w["date"])
+			out.write(',')
+			out.write(w["precipMM"])
+			out.write(',')
+			out.write(w["tempMinC"])
+			out.write(',')
+			out.write(w["tempMaxC"])
+			out.write(',')
+			out.write(w["weatherCode"])
+			out.write(',')
+			out.write(w["weatherDesc"][0]["value"])
+			out.write(',')
+			out.write(w["windspeedKmph"])
+			out.write("||")
+		}
+		out.write("\n")
+ 		sleep(1)
+	   end
 	end
 end
