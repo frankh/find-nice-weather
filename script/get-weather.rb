a8d3e1b08afb0913d1f0637aa215f7c4d7cb27b6
Frank Hamand <fhamand@geneity.co.uk>
2013-06-19 15:57:50 +0100

    changed script to read

--- script/get-weather.rb
+++ script/get-weather.rb
@@ -22,7 +22,7 @@ def ok_day(precipMM, tempMinC, tempMaxC, weatherCode, windspeedKmph)
 end
 
 def score_weather(*args)
-	args = args.collect{|x| x.to_i}
+	args = args.collect{|x| x.to_f}
 
 	if very_nice_day(*args)
 		return 1.1
@@ -34,39 +34,59 @@ def score_weather(*args)
 		return 0
 	end
 end
-max_score = -999
-counter = 0
-File.open("weather-geocoded-uk-towns", "w") do |out|
-	File.open("geocoded-uk-towns", "r") do |fd|
-		# skip first line
-		fd.gets
-		while (line = fd.gets)
-			town, lat, long = line.strip.split(/,/)
-			print town + "\n"
-			req_url = weather_url + "&q=" + lat+","+long+"&format=json"
-			resp = Net::HTTP.get_response('api.worldweatheronline.com', req_url)
-			json_resp = JSON.parse(resp.body)
-			out.write("#{town},#{lat},#{long}||")
-			total_score = 0
-			json_resp["data"]["weather"].each { |w| 
-				result_keys = ["date", "precipMM", "tempMinC", "tempMaxC", "weatherCode", "windspeedKmph"]
-				result_data = result_keys.collect{|x| w[x]}
-				this_score = score_weather(*result_data[1..6])
-				total_score += this_score
-				
-				out.write(result_data.join(","))
-				out.write("||")
-			}
-			out.write("\n")
-			print "Score is #{total_score}\n"
-			if total_score > max_score
-				max_score = total_score
-				print "Max score is #{max_score}\n"
+
+def collect_weather()
+	max_score = -999
+	counter = 0
+	File.open("weather-geocoded-uk-towns", "w") do |out|
+		File.open("geocoded-uk-towns", "r") do |fd|
+			# skip first line
+			fd.gets
+			while (line = fd.gets)
+				town, lat, long = line.strip.split(/,/)
+				print town + "\n"
+				req_url = weather_url + "&q=" + lat+","+long+"&format=json"
+				resp = Net::HTTP.get_response('api.worldweatheronline.com', req_url)
+				json_resp = JSON.parse(resp.body)
+				out.write("#{town},#{lat},#{long}||")
+				total_score = 0
+				json_resp["data"]["weather"].each { |w| 
+					result_keys = ["date", "precipMM", "tempMinC", "tempMaxC", "weatherCode", "windspeedKmph"]
+					result_data = result_keys.collect{|x| w[x]}
+					this_score = score_weather(*result_data[1..6])
+					total_score += this_score
+					
+					out.write(result_data.join(","))
+					out.write("||")
+				}
+				out.write("\n")
+				print "Score is #{total_score}\n"
+				if total_score > max_score
+					max_score = total_score
+					print "Max score is #{max_score}\n"
+				end
+				print "\n"
+				sleep(1)
 			end
-			print "\n"
-			sleep(1)
 		end
 	end
 end
 
+def read_weather()
+	File.open("weather-geocoded-uk-towns", "r") do |fd|
+		while (line = fd.gets)
+			town_info, today, *forecasts = line.strip.split(/\|\|/)
+			town, lat, long = town_info.strip.split(/,/)
 
+			total_score = 0
+			forecasts.each { |w| 
+				date, *data = w.strip.split(/,/).collect{|x| x.to_i}
+				total_score += score_weather(*data)
+			}
+
+			print town + "\t#{total_score}\n"
+		end
+	end
+end
+
+read_weather()
